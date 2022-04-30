d49c1e78f232c899f2b60781d7c8a249d1495539
Ranjan Singh Yadav <r.ranjan.singh.yadav@accenture.com>
2022-04-30 12:29:44 +0530

    first Commit

---
+++ src/Hel.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hel {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
