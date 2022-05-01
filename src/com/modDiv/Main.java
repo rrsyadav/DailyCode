256ee08fcef5e766e6f32a874f8fe9916d5b1654
Ranjan Singh Yadav <r.ranjan.singh.yadav@accenture.com>
2022-05-01 17:07:18 +0530

    First commit in branch1

---
+++ src/com/modDiv/Main.java
@@ -0,0 +1,20 @@
+package com.modDiv;
+
+    public class Main {
+        public static void main(String[] args) {
+            int  n=36638393;
+            int count =0;
+            while(n>0)
+            {
+                int  re=n%10;
+                if(re==3)
+                {
+                    count++;
+                }
+                n=n/10;
+            }
+            System.out.println(count);
+        }
+    }
+
+
