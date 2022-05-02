00063a1aa5a43fcbdf7b40573d949831164c329d
Ranjan Singh Yadav <r.ranjan.singh.yadav@accenture.com>
2022-05-02 13:07:35 +0530

    First Commit in branch 02/05/22

---
+++ src/com/calci/Main.java
@@ -0,0 +1,11 @@
+package com.calci;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner (System.in);
+        System.out.println("Enter the operator or press X to exit ");
+
+    }
+}
